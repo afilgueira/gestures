a99f483ca617a473880e890de6491806c6d0bc02
Adriano Filgueira <sradriano@gmail.com>
2018-07-10 00:13:06 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
 	$(gestures.container).on('swipeleft', function() {
-		var post_nav = jQuery('link[rel="next"]');
+		var post_nav = jQuery('.wp-post-navigation-next a');
 		if ( post_nav ) {
 			jQuery(location).attr('href', post_nav.attr('href'));
 		}		
 	}).on('swiperight', function() {
-		var post_nav = jQuery('link[rel="prev"]');
+		var post_nav = jQuery('.wp-post-navigation-pre a');
 		if ( post_nav ) {
 			jQuery(location).attr('href', post_nav.attr('href'));
 		}		
