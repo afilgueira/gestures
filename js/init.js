2976af194fb8a887128853ea4f4979b5e7501af8
Adriano Filgueira <sradriano@gmail.com>
2018-07-09 22:16:27 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,30 +1,14 @@
 		
 jQuery(document).ready(function($){
-
-	
-	$(gestures.container).on('dbltap', function(){
-		
-			jQuery(location).attr('href', gestures.destination );
-	
-		}
-	).on('swipeleft', function() {
-
-			var post_nav = jQuery('link[rel="next"]');
-		
-			if ( post_nav ) {
-				jQuery(location).attr('href', post_nav.attr('href'));
-			}		
-	
-		}	
-	).on('swiperight', function() {
-
-			var post_nav = jQuery('link[rel="prev"]');
-		
-			if ( post_nav ) {
-				jQuery(location).attr('href', post_nav.attr('href'));
-			}		
-	
-		}
-	);
-
+	$(gestures.container).on('swipeleft', function() {
+		var post_nav = "<?php next_post_link(); ?>";
+		if ( post_nav ) {
+			jQuery(location).attr('href', post_nav.attr('href'));
+		}		
+	}).on('swiperight', function() {
+		var post_nav = "<?php previous_post_link(); ?>";
+		if ( post_nav ) {
+			jQuery(location).attr('href', post_nav.attr('href'));
+		}		
+	});
 });
