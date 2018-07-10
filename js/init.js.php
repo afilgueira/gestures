82ad16c1b45985e6b909d33c3087ef1a546c3b11
Adriano Filgueira <sradriano@gmail.com>
2018-07-09 23:29:09 -0300

    Update and rename init.js to init.js.php

--- js/init.js.php
+++ js/init.js.php
@@ -1,12 +1,12 @@
 		
 jQuery(document).ready(function($){
 	$(gestures.container).on('swipeleft', function() {
-		var post_nav = "<?php next_post_link(); ?>";
+		var post_nav = <?php next_post_link(); ?>;
 		if ( post_nav ) {
 			jQuery(location).attr('href', post_nav.attr('href'));
 		}		
 	}).on('swiperight', function() {
-		var post_nav = "<?php previous_post_link(); ?>";
+		var post_nav = <?php previous_post_link(); ?>;
 		if ( post_nav ) {
 			jQuery(location).attr('href', post_nav.attr('href'));
 		}		
